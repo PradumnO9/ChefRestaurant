bb3b5e0ad033a5d5e1c2f0b33ca4bc7e89101031
PradumnO9 <pradumn2999@gmail.com>
2024-07-07 22:08:41 +0530

    Write test case for contact us page

--- src/__test__/Contact.test.js
+++ src/__test__/Contact.test.js
@@ -2,39 +2,42 @@ import { render, screen } from "@testing-library/react";
 import ContactUs from "../components/ContactUs";
 import "@testing-library/jest-dom"; // toBeInTheDocument coming from here
 
-test("Should load contact us component", () => {
-  render(<ContactUs />);
+describe("Contact Us test cases", () => {
+  test("Should load contact us component", () => {
+    render(<ContactUs />);
 
-  // Querying
-  const heading = screen.getByRole("heading");
+    // Querying
+    const heading = screen.getByRole("heading");
 
-  // Assertion
-  expect(heading).toBeInTheDocument();
-});
+    // Assertion
+    expect(heading).toBeInTheDocument();
+  });
 
-test("Should load Button inside contact component", () => {
-  render(<ContactUs />);
+  test("Should load Button inside contact component", () => {
+    render(<ContactUs />);
 
-  const button = screen.getByRole("button");
+    const button = screen.getByRole("button");
 
-  // Assertion
-  expect(button).toBeInTheDocument();
-});
+    // Assertion
+    expect(button).toBeInTheDocument();
+  });
 
-test("Should load input name inside contact component", () => {
-  render(<ContactUs />);
+  // it() and test() both are same only name different
+  it("Should load input name inside contact component", () => {
+    render(<ContactUs />);
 
-  const inputName = screen.getByPlaceholderText("name");
+    const inputName = screen.getByPlaceholderText("name");
 
-  // Assertion
-  expect(inputName).toBeInTheDocument();
-});
+    // Assertion
+    expect(inputName).toBeInTheDocument();
+  });
 
-test("Should load 2 input boxes inside contact component", () => {
-  render(<ContactUs />);
+  test("Should load 2 input boxes inside contact component", () => {
+    render(<ContactUs />);
 
-  const inputBoxes = screen.getAllByRole("textbox"); // for multiple items use getAll -> returns a array of object
+    const inputBoxes = screen.getAllByRole("textbox"); // for multiple items use getAll -> returns a array of object
 
-  // Assertion
-  expect(inputBoxes.length).toBe(2);
+    // Assertion
+    expect(inputBoxes.length).toBe(2);
+  });
 });
